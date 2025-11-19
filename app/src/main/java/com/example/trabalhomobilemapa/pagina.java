79ee7a16affba09e72d6ab331e23d327672b5496
Stephanievm <vmarquesstephanie@gmail.com>
2025-11-19 11:48:50 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/trabalhomobilemapa/pagina.java
@@ -0,0 +1,14 @@
+package com.example.trabalhomobilemapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pagina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina);
+    }
+}
